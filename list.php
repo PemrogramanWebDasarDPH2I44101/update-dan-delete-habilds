23c98bc1e5900ed2cec2f38f40619ad747d0e2ad
Andi Habil Dwi Syaputra <hbildsdsds@gmail.com>
2018-10-05 22:44:28 +0800

    Update list.php

--- list.php
+++ list.php
@@ -1,30 +1,44 @@
 <?php
-require_once("db.php");git 
+require_once("db.php");
 ?>
-<table border=1>
-    <thead>
-        <th>Nama</th>
-        <th>Nim</th>
-        <th>Tanggal Lahir</th>
-    </thead>
-    <tbody>
-<?php
-$sql    = "SELECT * FROM siswa";
-$result = mysqli_query($conn, $sql);
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data</title>
+</head>
+<body>
+    <h2 align="center">Semua Data</h2>
+    <table align="center" cellpadding="5" width="400px" border=1 style="border-collapse: collapse">
+        <tr align="left">
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Tgl Lahir</th>
+            <th><a href="form.php">Input Data</a></th>
+        </tr>
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row["nama"]. "</td>"; 
-        echo "<td>" . $row["nim"]. "</td>";
-        echo "<td>" . $row["tgl_lahir"]. "</td>";
-        echo "</tr>";
-    }
-} else {
-    echo "0 results";
-}
-mysqli_close($conn);
-?> 
-    </tbody>
-</table>
+        <?php
+            $query = "SELECT * FROM siswa";
+
+            $result = mysqli_query($connection, $query);
+            $row = mysqli_num_rows($result);
+
+            if ($row > 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    $id = $row["nim"];
+                    ?>
+                    <tr>
+                        <td><?php echo $row["nama"]; ?></td>
+                        <td><?php echo $row["nim"]; ?></td>
+                        <td><?php echo $row["tgl_lahir"]; ?></td>
+                        <td><?php echo "<a href='up_form.php?nim=$id'>Edit</a> | <a href='delete.php?nim=$id'>Hapus</a>"; ?></td>
+                    </tr>
+                    <?php
+                }
+            }else {
+                echo "<tr><td colspan='4' align='center'>0 results.</td></tr>";
+            }
+        ?>
+
+    </table>
+</body>
+</html>
